bc62eccd1022472bbc1188c37206dc8f8a6d9d78
XthemeCore <xthemecore@gmail.com>
2026-04-27 01:04:49 +0700

    Add support custom line height
    Plus include license in file

--- ThaiFontAutoAdjuster.js
+++ ThaiFontAutoAdjuster.js
@@ -6,6 +6,29 @@
  * @target MZ
  * @plugindesc ปรับตัวอักษรภาษาไทยไปตามยถากรรม
  * License: MIT License
+ * =============================================================================
+ * MIT License
+ *
+ * Copyright (c) 2020-Present XthemeCore
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * =============================================================================
  * @author XthemeCore
  * 
  * @param ======== Common ========
@@ -17,7 +40,23 @@
  * @desc ระยะพิกเซลของตัวอักษรที่ต้องการขยายออกไป (เช่นภาษาไทยที่มีวรรณยุกต์เหนือสระ)
  * @type number
  * @default 6
+ * 
+ * @param enableLineHeight
+ * @parent ======== Common ========
+ * @text Enable Line Height
+ * @desc เปิดใช้การแก้ไข Line Height หรือไม่
+ * @type boolean
+ * @on Enabled
+ * @off Disabled
+ * @default true
  *
+ * @param lineHeight
+ * @parent ======== Common ========
+ * @text Line Height
+ * @desc ความสูงของกล่องข้อความ
+ * @type number
+ * @default 42
+ * 
  * @help ThaiFontAutoAdjuster.js
  *
  * ปลั๊กอินนี้จะปรับตัวอักษรภาษาไทยให้เป็นไปตามยถากรรม
@@ -62,4 +101,17 @@
         y += parseInt(XTC.pixelYOffset || 6, 10);
         XTC_Bitmap_drawText.call(this, text, x, y, maxWidth, lineHeight, align);
     };
+
+    const XTC_Window_Base_lineHeight = Window_Base.prototype.lineHeight;
+    Window_Base.prototype.lineHeight = function() {
+        if (XTC.enableLineHeight)
+        {
+            return parseInt(XTC.lineHeight || 42, 10);
+        }
+        else
+        {
+            return XTC_Window_Base_lineHeight.call(this);
+        }
+    };
+
 })();
