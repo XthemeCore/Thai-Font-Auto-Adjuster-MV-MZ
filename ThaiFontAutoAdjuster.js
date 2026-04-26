6f347b3390b2639be5ce88c7c1573e012965b5de
XthemeCore <xthemecore@gmail.com>
2026-04-27 00:22:01 +0700

    Support MV
    Add support to RPG Maker MV and allow custom bitmap height

--- ThaiFontAutoAdjuster.js
+++ ThaiFontAutoAdjuster.js
@@ -2,14 +2,64 @@
 // Thai Font Automatic Adjuster
 //=============================================================================
 /*:
+ * @target MV
  * @target MZ
  * @plugindesc ปรับตัวอักษรภาษาไทยไปตามยถากรรม
  * License: MIT License
  * @author XthemeCore
+ * 
+ * @param ======== Common ========
+ * @default
+ * 
+ * @param pixelYOffset
+ * @parent ======== Common ========
+ * @text Font Bitmap Y Offset
+ * @desc ระยะพิกเซลของตัวอักษรที่ต้องการขยายออกไป (เช่นภาษาไทยที่มีวรรณยุกต์เหนือสระ)
+ * @type number
+ * @default 6
  *
  * @help ThaiFontAutoAdjuster.js
  *
  * ปลั๊กอินนี้จะปรับตัวอักษรภาษาไทยให้เป็นไปตามยถากรรม
  * การแสดงผลจะตรงกับที่แสดงในหน้าจอเริ่มเกมและเมนูต่างๆ 
  */
-(()=>{"use strict";Utils.containsThaiSpecialCharacters=function(e){return/[ำิีึืัํ่้๊๋์ฺุู็ๅ]/.test(e)};const s=Window_Base.prototype.processCharacter;Window_Base.prototype.processCharacter=function(e){var t=e.text[e.index],i=e.text[e.index+1],a=e.text[e.index+2];Utils.containsThaiSpecialCharacters(i)&&(e.buffer+=t,Utils.containsThaiSpecialCharacters(a)&&(e.buffer+=i,e.index++),e.index++),s.apply(this,arguments)};const n=Window_Message.prototype.processCharacter;Window_Message.prototype.processCharacter=function(e){var t=e.text[e.index],i=e.text[e.index+1],a=e.text[e.index+2];if(this.isShakingActive&&this.isShakingActive()&&!this._checkWordWrapMode){if(Imported.YEP_MessageCore&&this.checkWordWrap(e))return this.processNewLine(e);var s=t;"\n"!==s?Utils.containsThaiSpecialCharacters(i)&&(s+=i,Utils.containsThaiSpecialCharacters(a)&&(s+=a,e.index++)):(e.x=e.startX,e.y+=e.height),e.index++;var r=this.textWidth(s),c=e.height;this.createShakingCharacter(e,s,r,c),e.x+=r}else Utils.containsThaiSpecialCharacters(i)&&(e.buffer+=t,Utils.containsThaiSpecialCharacters(a)&&(e.buffer+=i,e.index++),e.index++),n.call(this,e)}})();
+(() => { 'use strict';    
+    var XTC = XTC || {};
+    XTC = PluginManager.parameters('ThaiFontAutoAdjuster');
+
+    Utils.containsThaiSpecialCharacters = function (character) {
+        return /[ำิีึืัํ่้๊๋์ฺุู็ๅ]/.test(character);
+    };
+
+    const XTC_Window_Base_processCharacter = Window_Base.prototype.processCharacter;
+    Window_Base.prototype.processCharacter = function (textState) {
+        console.trace(textState);
+        if(textState)
+        {
+            let index = textState.index;
+            if (Utils.containsThaiSpecialCharacters(textState.text[index + 1]))
+            {            
+                let buffer = textState.text[index] + textState.text[index + 1];
+                textState.index += 2;
+                if (Utils.containsThaiSpecialCharacters(textState.text[index + 2]))
+                {
+                    buffer += textState.text[index + 2]        
+                    textState.index++;
+                }
+                let width = this.textWidth(buffer);
+                this.contents.drawText(buffer, textState.x, textState.y, width * 2, textState.height);
+                textState.x += width;
+            } 
+            else
+            {
+                XTC_Window_Base_processCharacter.call(this, textState);
+            }
+        }
+    };
+
+    const XTC_Bitmap_drawText = Bitmap.prototype.drawText;
+    Bitmap.prototype.drawText = function (text, x, y, maxWidth, lineHeight, align) {
+        y += parseInt(XTC.pixelYOffset || 6, 10);
+        XTC_Bitmap_drawText.call(this, text, x, y, maxWidth, lineHeight, align);
+    };
+})();
